7adbec13deaae1c8fe6ac8adf54de68fe28af211
Alexandre Velozo <alexandrevelozo4@gmail.com>
2024-05-15 19:09:24 -0300

    feat: start frontend

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,10 +1,23 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../models/User";
 
 @Resolver()
 export class UserResolver {
+  private data: User[] = [];
 
-  @Query(() => String)
-  async hello() {
-    return "Hello World!"
+  @Query(() => [User])
+  async users() {
+    return this.data
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg('name') name: string
+  ) {
+    const user = { id: crypto.randomUUID(), name }
+
+    this.data.push(user)
+
+    return user;
   }
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { gql, useQuery } from "@apollo/client";
+import { NewUseForm } from "./components/NewUseForm";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type User = {
+  id: string;
+  name: string;
 }
 
-export default App
+export const GET_USER = gql`
+
+  query {
+    users {
+      id 
+      name
+    }
+  }
+`;
+
+export default function App() {
+  const { data, loading } = useQuery<{users: User[]}>(GET_USER);
+
+  if (loading) return <p>Carregando...</p>
+
+  return (
+    <div>
+      <ul>
+        {data?.users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+      <NewUseForm />
+    </div>
+  )
+}
